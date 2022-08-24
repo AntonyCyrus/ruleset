8c200ccc6a1979e7fa470636617cc76cf166ed62
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2022-08-24 23:12:30 +0800

    Update and rename ABC.py to 国外媒体.py

--- action/ABC.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-rawABC = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/ABC/ABC.yaml").text
-
-result = rawABC.split("\n")
-
-with open("./ABC.conf", "w") as f:
-    f.write("\n".join(result))
