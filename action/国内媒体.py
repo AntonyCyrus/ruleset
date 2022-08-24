b9d99ad215d5915daf86a69042d6f2de8ee9cf46
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2022-08-25 04:34:22 +0800

    Create 国内媒体.py

---
+++ action/国内媒体.py
@@ -0,0 +1,67 @@
+import requests
+
+raw115 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/115/115.yaml").text
+raw12306 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/12306/12306.yaml").text
+raw360 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/360/360.yaml").text
+raw58同城 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/58TongCheng/58TongCheng.yaml").text
+rawAcFun = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/AcFun/AcFun.yaml").text
+raw支付宝 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/AliPay/AliPay.yaml").text
+raw阿里巴巴 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Alibaba/Alibaba.yaml").text
+raw百度 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Baidu/Baidu.yaml").text
+raw百度贴吧 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/BaiDuTieBa/BaiDuTieBa.yaml").text
+rawBiliBili = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/BiliBili/BiliBili.yaml").text
+rawCCTV = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/CCTV/CCTV.yaml").text
+rawCSDN = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/CSDN/CSDN.yaml").text
+raw城通网盘 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/ChengTongWangPan/ChengTongWangPan.yaml").text
+rawChina = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/China/China.yaml").text
+rawChinaIPs = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/ChinaIPs/ChinaIPs_Classical.yaml").text
+rawChinaIPsBGP = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/ChinaIPsBGP/ChinaIPsBGP_Classical.yaml").text
+raw国内网站合集 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/ChinaMax/ChinaMax_Classical.yaml").text
+rawChinaMedia = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/ChinaMedia/ChinaMedia.yaml").text
+raw中国移动 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/ChinaMobile/ChinaMobile.yaml").text
+raw中国新闻网 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/ChinaNews/ChinaNews.yaml").text
+raw中国电信 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/ChinaTelecom/ChinaTelecom.yaml").text
+raw中国联通 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/ChinaUnicom/ChinaUnicom.yaml").text
+raw当当 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/DangDang/DangDang.yaml").text
+raw钉钉 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/DingTalk/DingTalk.yaml").text
+raw豆瓣 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/DouBan/DouBan.yaml").text
+raw抖音 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/DouYin/DouYin.yaml").text
+raw斗鱼 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Douyu/Douyu.yaml").text
+raw飞猪 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/FeiZhu/FeiZhu.yaml").text
+raw凤凰网 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/FengHuangWang/FengHuangWang.yaml").text
+raw高德 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/GaoDe/GaoDe.yaml").text
+rawGovernmentCN = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/GovCN/GovCN.yaml").text
+raw盒马 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/HeMa/HeMa.yaml").text
+raw虎牙 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/HuYa/HuYa.yaml").text
+raw华为 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Huawei/Huawei.yaml").text
+raw虎扑 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Hupu/Hupu.yaml").text
+raw爱奇艺 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/iQIYI/iQIYI.yaml").text
+raw坚果云 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/JianGuoYun/JianGuoYun.yaml").text
+raw简书 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/JianShu/JianShu.yaml").text
+raw京东 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/JingDong/JingDong.yaml").text
+raw快手 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/KuaiShou/KuaiShou.yaml").text
+raw酷狗/酷我 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/KugouKuwo/KugouKuwo.yaml").text
+rawOPPO = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/OPPO/OPPO.yaml").text
+rawOnePlus = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/OnePlus/OnePlus.yaml").text
+raw拼多多 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Pinduoduo/Pinduoduo.yaml").text
+raw平安 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/PingAn/PingAn.yaml").text
+raw新浪 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Sina/Sina.yaml").text
+raw搜狐 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Sohu/Sohu.yaml").text
+rawSteamCN = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/SteamCN/SteamCN.yaml").text
+raw苏宁 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/SuNing/SuNing.yaml").text
+raw腾讯 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Tencent/Tencent.yaml").text
+raw腾讯视频 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/TencentVideo/TencentVideo.yaml").text
+raw中国银联 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/UnionPay/UnionPay.yaml").text
+rawVivo = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Vivo/Vivo.yaml").text
+raw微信 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/WeChat/WeChat.yaml").text
+raw微博 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Weibo/Weibo.yaml").text
+raw闲鱼 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/XianYu/XianYu.yaml").text
+raw小米 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/XiaoMi/XiaoMi.yaml").text
+raw迅雷 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Xunlei/Xunlei.yaml").text
+raw优酷 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Youku/Youku.yaml").text
+raw知乎 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Zhihu/Zhihu.yaml").text
+
+result = raw115.split("\n") + raw12306.split("\n") + raw360.split("\n") + raw58同城.split("\n") + rawAcFun.split("\n") + raw支付宝.split("\n") + raw阿里巴巴.split("\n") + raw百度.split("\n") + raw百度贴吧.split("\n") + rawBiliBili.split("\n") + rawCCTV.split("\n") + rawCSDN.split("\n") + raw城通网盘.split("\n") + rawChina.split("\n") + rawChinaIPs.split("\n") + rawChinaIPsBGP.split("\n") + raw国内网站合集.split("\n") + rawChinaMedia.split("\n") + raw中国移动.split("\n") + raw中国新闻网.split("\n") + raw中国电信.split("\n") + raw中国联通.split("\n") + raw当当.split("\n") + raw钉钉.split("\n") + raw豆瓣.split("\n") + raw抖音.split("\n") + raw斗鱼.split("\n") + raw飞猪.split("\n") + raw凤凰网.split("\n") + raw高德.split("\n") + rawGovernmentCN.split("\n") + raw盒马.split("\n") + raw虎牙.split("\n") + raw华为.split("\n") + raw虎扑.split("\n") + raw爱奇艺.split("\n") + raw坚果云.split("\n") + raw简书.split("\n") + raw京东.split("\n") + raw快手.split("\n") + raw酷狗/酷我.split("\n") + rawOPPO.split("\n") + rawOnePlus.split("\n") + raw拼多多.split("\n") + raw平安.split("\n") + raw新浪.split("\n") + raw搜狐.split("\n") + rawSteamCN.split("\n") + raw苏宁.split("\n") + raw腾讯.split("\n") + raw腾讯视频.split("\n") + raw中国银联.split("\n") + rawVivo.split("\n") + raw微信.split("\n") + raw微博.split("\n") + raw闲鱼.split("\n") + raw小米.split("\n") + raw迅雷.split("\n") + raw优酷.split("\n") + raw知乎.split("\n")
+
+with open("./国内媒体.yaml", "w") as f:
+    f.write("\n".join(result))
