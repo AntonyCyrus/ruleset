ed18cbb73be1893634b9d36e4b7ccb141684df7d
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2022-08-30 11:03:17 +0800

    Update 国外媒体.py

--- action/国外媒体.py
+++ action/国外媒体.py
@@ -83,5 +83,5 @@ for rawresult in [rawAbemaTV, rawAll4, rawAmazon, rawAmazonPrimeVideo, rawApple,
     result.extend([item.rstrip() for item in rawresult.split('\n') if not (item.startswith('#') or item.startswith('payload:'))])
 result_text = '\n'.join(result)
 
-with open("./国外媒体.yaml", "w") as f:
+with open("./ProxyMedia.yaml", "w") as f:
     f.write("\n".join(result))
